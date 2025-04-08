1a57d28740ff0df58a9d647d214457878167f77f
elvissantos7 <elvispereira647@gmail.com>
2025-04-08 23:06:30 +0000

    .

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+typedef struct {
+    char codigo[4]; // Código da carta (ex: A01)
+    int populacao;  // População
+    float area;     // Área
+    float pib;      // PIB
+    int pontos_turisticos; // Número de pontos turísticos
+} Carta;
 
 int main() {
 
