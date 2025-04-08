ebd6a4466a213a0e8cb501bc0bc6d41e843e1bf3
elvissantos7 <elvispereira647@gmail.com>
2025-04-08 23:08:12 +0000

    ,

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,50 @@ typedef struct {
 
 int main() {
 
+    Carta carta1, carta2;
+
+    // Cadastro da primeira carta
+    printf("Cadastro da primeira carta:\n");
+    printf("Digite o código da carta (ex: A01): ");
+    scanf("%s", carta1.codigo);
+    printf("Digite a população: ");
+    scanf("%d", &carta1.populacao);
+    printf("Digite a área: ");
+    scanf("%f", &carta1.area);
+    printf("Digite o PIB: ");
+    scanf("%f", &carta1.pib);
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &carta1.pontos_turisticos);
+
+    // Cadastro da segunda carta
+    printf("\nCadastro da segunda carta:\n");
+    printf("Digite o código da carta (ex: B02): ");
+    scanf("%s", carta2.codigo);
+    printf("Digite a população: ");
+    scanf("%d", &carta2.populacao);
+    printf("Digite a área: ");
+    scanf("%f", &carta2.area);
+    printf("Digite o PIB: ");
+    scanf("%f", &carta2.pib);
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &carta2.pontos_turisticos);
+
+    // Exibição dos dados da primeira carta
+    printf("\nDados da primeira carta:\n");
+    printf("Código: %s\n", carta1.codigo);
+    printf("População: %d\n", carta1.populacao);
+    printf("Área: %.2f\n", carta1.area);
+    printf("PIB: %.2f\n", carta1.pib);
+    printf("Pontos turísticos: %d\n", carta1.pontos_turisticos);
+
+    // Exibição dos dados da segunda carta
+    printf("\nDados da segunda carta:\n");
+    printf("Código: %s\n", carta2.codigo);
+    printf("População: %d\n", carta2.populacao);
+    printf("Área: %.2f\n", carta2.area);
+    printf("PIB: %.2f\n", carta2.pib);
+    printf("Pontos turísticos: %d\n", carta2.pontos_turisticos);
+
 
     return 0;
 }
